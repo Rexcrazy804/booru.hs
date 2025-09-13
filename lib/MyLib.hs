36c9f73e2d4ac44d9c538a20520946f823b55538
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-09-13 17:59:35 +0400

    feat: basic working state for toml-parser lib

--- lib/MyLib.hs
+++ lib/MyLib.hs
@@ -1,4 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module MyLib (someFunc) where
 
+import Data.Text
+import QuoteStr (quoteStr)
+import Toml (parse)
+
+fruitStr :: Text
+fruitStr =
+  [quoteStr|
+  [[fruits]]
+  name = "apple"
+
+  [fruits.physical]  # subtable
+  color = "red"
+  shape = "round"
+
+  [[fruits.varieties]]  # nested array of tables
+  name = "red delicious"
+
+  [[fruits.varieties]]
+  name = "granny smith"
+
+  [[fruits]]
+  name = "banana"
+
+  [[fruits.varieties]]
+  name = "plantain"
+  |]
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  print $ parse fruitStr
