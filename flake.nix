d0ff7f2d3d81ee74137067f92d8b1aca7cd38136
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-09-13 18:13:59 +0400

    feat: added ifd package for rapid development

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   description = "A minimal Flake template";
 
+  nixConfig.allow-import-from-derivation = true;
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     systems.url = "github:nix-systems/default";
@@ -18,8 +20,10 @@
     eachSystem = fn: mapAttrs fn pkgsFor;
   in {
     formatter = eachSystem (_: pkgs: pkgs.alejandra);
-    packages = eachSystem (_: pkgs: {
-      default = pkgs.haskellPackages.callPackage ./nix/package.nix {};
+    packages = eachSystem (system: pkgs: {
+      noifd = pkgs.haskellPackages.callPackage ./nix/package.nix {};
+      rapid = pkgs.haskellPackages.callCabal2nix "booru-hs" (self.packages.${system}.noifd.src) {};
+      default = self.packages.${system}.noifd;
     });
     devShells = eachSystem (_: pkgs: {
       default = pkgs.callPackage ./nix/shell.nix {inherit self;};
