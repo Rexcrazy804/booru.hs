f5b4dee56d78c2e34501f44f8aa067b657ba074f
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-09-14 17:15:20 +0400

    feat: initalization of test suite

---
+++ test/SourceParseSpec.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module SourceParseSpec (spec) where
+
+import Booru.Schema.Sources
+import Data.Text (Text)
+import QuoteStr (quoteStr)
+import Test.Hspec (Spec, it, shouldBe)
+import Toml (Result (..), decode)
+
+source :: Text
+source =
+  [quoteStr|
+  [[sources]]
+  provider = "myprovider"
+  ids = ["12 kok", "13", "14"]
+
+  [[sources.overrides]]
+  identifier = "kok"
+  append = true
+  characters = ["kokomi"]
+  copyrights = ["genshin_impact"]
+  tags = ["jellyfish", "underwater"]
+  ratings = ["explicit"]
+
+  [sources.filters]
+  characters = { list = ["abc", "xyz"], inverted = false }
+  copyrights = { list = ["arknights"], inverted = false }
+  artists = { list = ["mourncolor", "elodias"], inverted = true }
+  tags = { list = ["bird", "horse"], inverted = false }
+  ids = { list = ["11112"], inverted = false }
+  ratings = { list = ["g"], inverted = true }
+
+  [sources.previews]
+  enabled = true
+  characters = { list = ["abc", "xyz"], inverted = false }
+  copyrights = { list = ["arknights"], inverted = false }
+  artists = { list = ["mourncolor", "elodias"], inverted = true }
+  tags = { list = ["bird", "horse"], inverted = false }
+  ids = { list = ["11112"], inverted = false }
+  ratings = { list = ["g"], inverted = true }
+  |]
+
+spec :: Spec
+spec = do
+  it "parses Source" $
+    decode source
+      `shouldBe` ( Success [] $
+                    Sources
+                      { sources =
+                          [ ( Source
+                                { provider = "myprovider"
+                                , ids = ["12 kok", "13", "14"]
+                                , overrides =
+                                    Just
+                                      [ ( Override
+                                            { identifier = "kok"
+                                            , append = True
+                                            , artists = Nothing
+                                            , characters = Just ["kokomi"]
+                                            , copyrights = Just ["genshin_impact"]
+                                            , tags = Just ["jellyfish", "underwater"]
+                                            , ratings = Just ["explicit"]
+                                            }
+                                        )
+                                      ]
+                                , filters =
+                                    Just $
+                                      Filters
+                                        { fcharacters = Just Filter{list = ["abc", "xyz"], inverted = False}
+                                        , fcopyrights = Just Filter{list = ["arknights"], inverted = False}
+                                        , fartists = Just Filter{list = ["mourncolor", "elodias"], inverted = True}
+                                        , ftags = Just Filter{list = ["bird", "horse"], inverted = False}
+                                        , fids = Just Filter{list = ["11112"], inverted = False}
+                                        , fratings = Just Filter{list = ["g"], inverted = True}
+                                        }
+                                , previews =
+                                    Just $
+                                      Previews
+                                        { enabled = True
+                                        , pcharacters = Just Filter{list = ["abc", "xyz"], inverted = False}
+                                        , pcopyrights = Just Filter{list = ["arknights"], inverted = False}
+                                        , partists = Just Filter{list = ["mourncolor", "elodias"], inverted = True}
+                                        , ptags = Just Filter{list = ["bird", "horse"], inverted = False}
+                                        , pids = Just Filter{list = ["11112"], inverted = False}
+                                        , pratings = Just Filter{list = ["g"], inverted = True}
+                                        }
+                                }
+                            )
+                          ]
+                      }
+                 )
