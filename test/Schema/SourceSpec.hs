512b96f52d2ca4a54f091aa08e1499d33633d865
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-09-14 17:36:10 +0400

    style: improved readability of SourceSpec

--- test/Schema/SourceSpec.hs
+++ test/Schema/SourceSpec.hs
@@ -8,8 +8,8 @@ import QuoteStr (quoteStr)
 import Test.Hspec (Spec, it, shouldBe)
 import Toml (Result (..), decode)
 
-fullsource :: Text
-fullsource =
+fullSource :: Text
+fullSource =
   [quoteStr|
   [[sources]]
   provider = "myprovider"
@@ -41,6 +41,52 @@ fullsource =
   ratings = { list = ["g"], inverted = true }
   |]
 
+fullSourceParsed :: Sources
+fullSourceParsed =
+  Sources
+    { sources =
+        [ ( Source
+              { provider = "myprovider"
+              , ids = ["12 kok", "13", "14"]
+              , overrides =
+                  Just
+                    [ ( Override
+                          { identifier = "kok"
+                          , append = True
+                          , artists = Nothing
+                          , characters = Just ["kokomi"]
+                          , copyrights = Just ["genshin_impact"]
+                          , tags = Just ["jellyfish", "underwater"]
+                          , ratings = Just ["explicit"]
+                          }
+                      )
+                    ]
+              , filters =
+                  Just
+                    Filters
+                      { fcharacters = Just Filter{list = ["abc", "xyz"], inverted = False}
+                      , fcopyrights = Just Filter{list = ["arknights"], inverted = False}
+                      , fartists = Just Filter{list = ["mourncolor", "elodias"], inverted = True}
+                      , ftags = Just Filter{list = ["bird", "horse"], inverted = False}
+                      , fids = Just Filter{list = ["11112"], inverted = False}
+                      , fratings = Just Filter{list = ["g"], inverted = True}
+                      }
+              , previews =
+                  Just
+                    Previews
+                      { enabled = True
+                      , pcharacters = Just Filter{list = ["abc", "xyz"], inverted = False}
+                      , pcopyrights = Just Filter{list = ["arknights"], inverted = False}
+                      , partists = Just Filter{list = ["mourncolor", "elodias"], inverted = True}
+                      , ptags = Just Filter{list = ["bird", "horse"], inverted = False}
+                      , pids = Just Filter{list = ["11112"], inverted = False}
+                      , pratings = Just Filter{list = ["g"], inverted = True}
+                      }
+              }
+          )
+        ]
+    }
+
 emptySource :: Text
 emptySource =
   [quoteStr|
@@ -49,69 +95,22 @@ emptySource =
   ids = ["834871"]
   |]
 
+emptySourceParsed :: Sources
+emptySourceParsed =
+  Sources
+    { sources =
+        [ ( Source
+              { provider = "danbooru"
+              , ids = ["834871"]
+              , overrides = Nothing
+              , filters = Nothing
+              , previews = Nothing
+              }
+          )
+        ]
+    }
+
 spec :: Spec
 spec = do
-  it "parses exhaustive Source.toml" $
-    decode fullsource
-      `shouldBe` Success
-        []
-        Sources
-          { sources =
-              [ ( Source
-                    { provider = "myprovider"
-                    , ids = ["12 kok", "13", "14"]
-                    , overrides =
-                        Just
-                          [ ( Override
-                                { identifier = "kok"
-                                , append = True
-                                , artists = Nothing
-                                , characters = Just ["kokomi"]
-                                , copyrights = Just ["genshin_impact"]
-                                , tags = Just ["jellyfish", "underwater"]
-                                , ratings = Just ["explicit"]
-                                }
-                            )
-                          ]
-                    , filters =
-                        Just $
-                          Filters
-                            { fcharacters = Just Filter{list = ["abc", "xyz"], inverted = False}
-                            , fcopyrights = Just Filter{list = ["arknights"], inverted = False}
-                            , fartists = Just Filter{list = ["mourncolor", "elodias"], inverted = True}
-                            , ftags = Just Filter{list = ["bird", "horse"], inverted = False}
-                            , fids = Just Filter{list = ["11112"], inverted = False}
-                            , fratings = Just Filter{list = ["g"], inverted = True}
-                            }
-                    , previews =
-                        Just $
-                          Previews
-                            { enabled = True
-                            , pcharacters = Just Filter{list = ["abc", "xyz"], inverted = False}
-                            , pcopyrights = Just Filter{list = ["arknights"], inverted = False}
-                            , partists = Just Filter{list = ["mourncolor", "elodias"], inverted = True}
-                            , ptags = Just Filter{list = ["bird", "horse"], inverted = False}
-                            , pids = Just Filter{list = ["11112"], inverted = False}
-                            , pratings = Just Filter{list = ["g"], inverted = True}
-                            }
-                    }
-                )
-              ]
-          }
-
-  it "parses empty source" $
-    decode emptySource
-      `shouldBe` Success
-        []
-        Sources
-          { sources =
-              [ ( Source
-                    { provider = "danbooru"
-                    , ids = ["834871"]
-                    , overrides = Nothing
-                    , filters = Nothing
-                    , previews = Nothing
-                    }
-                )
-              ]
-          }
+  it "parses exhaustive Source" $ decode fullSource `shouldBe` Success [] fullSourceParsed
+  it "parses empty source" $ decode emptySource `shouldBe` Success [] emptySourceParsed
