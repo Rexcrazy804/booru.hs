22eb54a24e4f88e99fd5ec8d4c3e2db18cac0d00
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-09-15 19:27:27 +0400

    test: network features

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import Data.Aeson (Value)
+import Network.HTTP.Simple (getResponseBody, httpJSON, parseRequest, setRequestHeader)
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
+  request' <- parseRequest "https://danbooru.donmai.us/posts/9881008.json"
+  let request = setRequestHeader "User-Agent" ["curl/8.14.1"] request'
+  response <- httpJSON request
+  print (getResponseBody response :: Maybe Value)
