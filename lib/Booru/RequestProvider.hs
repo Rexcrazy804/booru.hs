1b9932431b474ba995417419f2e765f09ff463cc
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-09-16 14:09:03 +0400

    docs(lib): document RequestProvider functions

--- lib/Booru/RequestProvider.hs
+++ lib/Booru/RequestProvider.hs
@@ -27,6 +27,21 @@ getProviderMap Providers{providers = prs} = fromList $ foldl mkProviderFns [] pr
  where
   mkProviderFns acc p@(Provider{name = n}) = (n, resolveProvider p) : acc
 
+{- |
+# Accepts a provider and an identifier to then return the Image.
+- this function is partially applied in the `getProviderMap`
+  to yeild a function that takes an identifier and returns the
+  `Image`
+
+## Cases
+This functional dynamically procs a json request for those
+providers that do not come with a `file` attribute.
+It then forwards the call down to the `extractImage`
+
+However, in the event there is no such attribute,
+the provider and the id is directly passed onto
+extractImage with `Nothing` for `Maybe Object`
+-}
 resolveProvider :: Provider -> Identifier -> IO (Maybe Image)
 resolveProvider prvdr@Provider{url = u, file = Just _} id' = do
   let url' = replace (pack "%%ID%%") (pack $ extractId id') (pack u)
@@ -34,6 +49,12 @@ resolveProvider prvdr@Provider{url = u, file = Just _} id' = do
   return $ extractImage prvdr id' raw
 resolveProvider prvdr id' = return $ extractImage prvdr id' Nothing
 
+{- |
+# Accepts a url, and potentially returns a json object
+If the response contains an object, the object is returned.
+however, in the event where a non empty array of objects is returned,
+the function returns the frist object in the array
+-}
 requestJson :: String -> IO (Maybe Object)
 requestJson url' = do
   request' <- parseRequest url'
@@ -49,7 +70,24 @@ requestJson url' = do
       _ -> Nothing
     _ -> Nothing
 
-extractImage :: Provider -> Maybe Object -> Maybe Image
+{- |
+# Constructs the final `Image` representation
+Accepts Provider Identifier and a potential json object
+to construct information regarding an image into `Image`
+
+## Cases
+Two primary casses come into play here,
+most providers will facilitate a file attribute
+and in turn the provider url is used initially
+to fetch information regarding the image.
+This information is retreived as a json object,
+and the `Provider` strucutre is used to extract information out of it.
+
+On the other hand, for providers like the `urls` provider,
+there is no json information to be fetched an inferred,
+hence extract image populates default information (provided with $<value>).
+and specially processes the provider url substituing %%img%% with id as requried
+-}
 extractImage :: Provider -> Identifier -> Maybe Object -> Maybe Image
 extractImage
   Provider
