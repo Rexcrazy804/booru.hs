0ce889fdfd591ff90afd1f6a6d2a7a24c6a697bb
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-09-16 12:40:03 +0400

    feat(lib): addition of extractId helper

--- lib/Helpers.hs
+++ lib/Helpers.hs
@@ -1,4 +1,8 @@
+{-# LANGUAGE DisambiguateRecordFields #-}
+
 module Helpers where
+
+import Booru.Schema.Sources (Identifier (..))
 
 wordsBy :: String -> (Char -> Bool) -> [String]
 wordsBy str delim = aux str
@@ -7,3 +11,7 @@ wordsBy str delim = aux str
   aux s = case dropWhile delim s of
     "" -> []
     s' -> let (w, s'') = break delim s' in w : aux s''
+
+extractId :: Identifier -> String
+extractId (Id x) = x
+extractId WithNick{id = x} = x
