6d733dd4766fbe6b43e759ec7e84ab6482614508
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-09-20 01:20:14 +0400

    feat(lib): added filter category unit

---
+++ lib/Booru/Core/FilterCat.hs
@@ -0,0 +1,18 @@
+module Booru.Core.FilterCat (filterCategory) where
+
+import Booru.Core.Category (Category (..), TagMap)
+import Booru.Schema.Filters (Filter (..), Filters (..))
+import Data.Bits (xor)
+import Data.Map (filterWithKey)
+
+filterCategory :: Filters -> Category -> Category
+filterCategory ft cat =
+  Category
+    { artistC = filterCatField (artists ft) (artistC cat)
+    , copyrightC = filterCatField (copyrights ft) (copyrightC cat)
+    , characterC = filterCatField (characters ft) (characterC cat)
+    }
+
+filterCatField :: Maybe Filter -> TagMap -> TagMap
+filterCatField (Just Filter{list = fts, inverted = inv}) = filterWithKey (\k _ -> not inv `xor` (k `elem` fts))
+filterCatField Nothing = id
