49b697de4ed061d005cc01bf05510d7f20248e81
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-09-19 13:19:48 +0400

    feat(lib): seperation of Filters from Sources

---
+++ lib/Booru/Schema/Filters.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingVia #-}
+
+module Booru.Schema.Filters (
+  Filter (..),
+  Filters (..),
+  Previews,
+)
+where
+
+import Booru.Schema.Images (Tag)
+import GHC.Generics (Generic)
+import Toml.Schema
+
+data Filter = Filter
+  { list :: [Tag]
+  , inverted :: Bool
+  }
+  deriving (Eq, Show, Generic)
+  deriving (ToTable, ToValue, FromValue) via GenericTomlTable Filter
+
+data Filters = Filters
+  { characters :: Maybe Filter
+  , copyrights :: Maybe Filter
+  , artists :: Maybe Filter
+  , tags :: Maybe Filter
+  , ids :: Maybe Filter
+  , ratings :: Maybe Filter
+  }
+  deriving (Eq, Show, Generic)
+  deriving (ToTable, ToValue, FromValue) via GenericTomlTable Filters
+
+type Previews = Filters
--- lib/Booru/Schema/Sources.hs
+++ lib/Booru/Schema/Sources.hs
@@ -6,11 +6,6 @@ module Booru.Schema.Sources (
   Sources (..),
   Source (..),
   Override (..),
-  Filters (..),
-  Filter (..),
-  Previews (..),
-  Identifier (..),
-  Tag,
 )
 where
 
@@ -28,8 +23,6 @@ data Source = Source
   , overrides :: Maybe [Override]
   }
   deriving (Eq, Show, Generic)
-  , filters :: Maybe Filters
-  , previews :: Maybe Previews
   deriving (ToTable, ToValue, FromValue) via GenericTomlTable Source
 
 data Override = Override
@@ -43,44 +36,3 @@ data Override = Override
   }
   deriving (Eq, Show, Generic)
   deriving (ToTable, ToValue, FromValue) via GenericTomlTable Override
-
-data Filter = Filter
-  { list :: [Tag]
-  , inverted :: Bool
-  }
-  deriving (Eq, Show, Generic)
-  deriving (ToTable, ToValue, FromValue) via GenericTomlTable Filter
-
-data Filters = Filters
-  { characters :: Maybe Filter
-  , copyrights :: Maybe Filter
-  , artists :: Maybe Filter
-  , tags :: Maybe Filter
-  , ids :: Maybe Filter
-  , ratings :: Maybe Filter
-  }
-  deriving (Eq, Show, Generic)
-  deriving (ToTable, ToValue, FromValue) via GenericTomlTable Filters
-
-data Previews = Previews
-  { enabled :: Bool
-  , filters :: Maybe Filters
-  }
-  deriving (Eq, Show, Generic)
-  deriving (ToTable, ToValue, FromValue) via GenericTomlTable Previews
-
-data Identifier = Id String | WithNick {id :: String, nickname :: String}
-  deriving (Eq, Show, Generic)
-
-instance ToValue Identifier where
-  toValue (Id id') = toValue id'
-  toValue (WithNick id' nick) = toValue $ unwords [id', nick]
-
-instance FromValue Identifier where
-  fromValue (Text' l packed)
-    | [y, z] <- x = return $ WithNick{id = y, nickname = z}
-    | [x'] <- x = return $ Id x'
-    | otherwise = failAt l "Expected a string of '<id>' or '<id> <nickname>'"
-   where
-    x = words $ unpack packed
-  fromValue _ = fail "expected a string"
