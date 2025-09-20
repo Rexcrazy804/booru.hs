fcdf0f775f762e7d4e9e410a76cc2ff1277300f3
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-09-20 20:32:44 +0400

    feat(lib): exported function to parse config

--- lib/Booru/Schema/Config.hs
+++ lib/Booru/Schema/Config.hs
@@ -2,14 +2,24 @@
 {-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 
-module Booru.Schema.Config where
+module Booru.Schema.Config (
+  -- re-exporting Toml parser's Result type
+  Result (..),
+  Config (..),
+  parseConfig,
+) where
 
 import Booru.Schema.Filters (Filters)
 import Booru.Schema.PFilters (PFilters)
 import Booru.Schema.Providers (Provider)
 import Booru.Schema.Sources (Source)
 import Booru.Schema.Synonyms (Synonyms)
+
+import System.IO (IOMode (ReadMode), hGetContents', withFile)
+
+import Data.Text (pack)
 import GHC.Generics (Generic)
+import Toml (decode)
 import Toml.Schema
 
 data Config = Config
@@ -21,3 +31,8 @@ data Config = Config
   }
   deriving (Eq, Show, Generic)
   deriving (ToTable, ToValue, FromValue) via GenericTomlTable Config
+
+parseConfig :: String -> IO (Result String Config)
+parseConfig cfgFile = do
+  todoData <- withFile cfgFile ReadMode hGetContents'
+  return $ decode (pack todoData)
