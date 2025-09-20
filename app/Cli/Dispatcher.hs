3859c976cfdfdb7b9c3279ddfaaa41a44bd52f41
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-09-20 20:05:21 +0400

    wip: cli p9 lil bit more docs

--- app/Cli/Dispatcher.hs
+++ app/Cli/Dispatcher.hs
@@ -3,6 +3,7 @@ module Cli.Dispatcher (dispatch) where
 import Cli.Commands.Download
 import Cli.Options
 
+-- | reads the options structure and dispatches subcommands
 dispatch :: Options -> IO ()
 dispatch Options{subcommand = Download opts} = getImages opts
 dispatch _ = return ()
