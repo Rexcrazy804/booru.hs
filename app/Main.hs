8c0b55ee3ab7f92573ab147d114b29542113138f
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-09-20 13:27:23 +0400

    wip: cli

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,28 @@
 module Main where
 
+import Booru.Builtin.Providers (danbooruDonmaiUs)
+import Booru.Core.Requests (requestFile, resolveProvider)
+import Booru.Schema.Images (
+  Identifier (Id),
+  Image (Image, resolvedName),
+  resolvedName,
+ )
+import Control.Monad (forM, forM_)
+import qualified Data.ByteString as L
+import Data.Maybe (catMaybes)
+import System.Environment (getArgs)
+
 main :: IO ()
 main = do
-  print "Hello world!!"
+  ids <- getArgs
+  let booruFetcher = resolveProvider danbooruDonmaiUs
+      idnfrs = map Id ids
+  imgs' <- forM idnfrs booruFetcher
+  let imgs = catMaybes imgs'
+  forM_ imgs downloadImage
+
+downloadImage :: Image -> IO ()
+downloadImage img@Image{resolvedName = name} = do
+  rsbod <- requestFile img
+  L.writeFile name rsbod
+  return ()
