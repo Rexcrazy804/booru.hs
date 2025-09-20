1c5cad5aa13867e2de57ff7c0dbbb96c93c8b119
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-09-20 23:31:10 +0400

    fix(app): leveraged common extractCfg + comply with commonOpts

--- app/Cli/Dispatcher.hs
+++ app/Cli/Dispatcher.hs
@@ -1,32 +1,15 @@
 module Cli.Dispatcher (dispatch) where
 
-import Booru.Schema.Config (Config, Result (..), parseConfig)
 import Cli.Commands.Download
+import Cli.Common
 import Cli.Options
-import System.Environment.XDG.BaseDir (getUserConfigDir)
-import System.FilePath ((</>))
+import Control.Monad ((<=<))
 
 -- | reads the options structure and dispatches subcommands
 dispatch :: Options -> IO ()
-dispatch Options{subcommand = sub, config = cfg} = do
-  conf <- extractCfg cfg
-  runSubCommand conf sub
+dispatch Options{subcommand = sub, common = copts} = do
+  runSubCommand sub copts
 
-runSubCommand :: Config -> Commands -> IO ()
-runSubCommand cfg (Download opts) = getImages opts cfg
-runSubCommand cfg _ = print cfg
-
-extractCfg :: String -> IO Config
-extractCfg cfg = do
-  booruDir <- getUserConfigDir "booru"
-  let
-    defaultCfg = booruDir </> "config.toml"
-    defaultCfg' = "$XDG_CONFIG_HOME/booru/config.toml"
-    cfg' = if cfg == defaultCfg' then defaultCfg else cfg
-  rs <- parseConfig cfg'
-  case rs of
-    Failure e -> fail $ unwords $ map ("[Error] config: " ++) e
-    Success [] c -> return c
-    Success wns c -> do
-      putStrLn $ unlines $ map ("[Warning] config: " ++) wns
-      return c
+runSubCommand :: Commands -> CommonOpts -> IO ()
+runSubCommand (Download opts) = getImages opts
+runSubCommand _ = (print <=< extractCfg) . configDir
