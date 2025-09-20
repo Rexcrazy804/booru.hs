d4ed07277e7bfd56d32f99bda51bfcf29d17bf6d
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-09-20 20:00:45 +0400

    wip: cli p8 documenting + style

--- app/Cli/Options.hs
+++ app/Cli/Options.hs
@@ -7,24 +7,22 @@ module Cli.Options (
 
 import Options.Applicative
 
-data Options = Options
-  { subcommand :: Commands
-  , config :: String
-  }
-
-data Commands = Build | Download DownloadOpts
-
-data DownloadOpts = DownloadOpts
-  { provider :: String
-  , ids :: [String]
-  }
-
+-- | parses command line arguments given to the program into the Options record
 parseOpts :: IO Options
 parseOpts =
   execParser $
     info
       (optionParser <**> helper)
       (fullDesc <> header "Booru-hs Cli for booru needs")
+
+{- | # Options Record
+**subcommand:** stores the subcommand to run
+**config:** contains the path to config.toml
+-}
+data Options = Options
+  { subcommand :: Commands
+  , config :: String
+  }
 
 optionParser :: Parser Options
 optionParser =
@@ -38,15 +36,30 @@ optionParser =
           <> help "Toml file containing booru-hs configuration"
       )
 
+-- | Available subcommands and their option records
+data Commands
+  = Build
+  | Download DownloadOpts
+
+-- | scaffolds logic for parsing sub commands
+commandsParser :: Parser Commands
+commandsParser =
+  hsubparser $
+    command "build" (info (pure Build) $ progDesc "build the image folder")
+      <> command "download" (info (Download <$> dlOptParser) $ progDesc "download images using IDS from a given PROVIDER")
+
+{- | # Download subcommand Options
+**provider:** selected provider to rquest image and metagdata from
+**ids:** list of strings containing ids to request
+-}
+data DownloadOpts = DownloadOpts
+  { provider :: String
+  , ids :: [String]
+  }
+
+-- | don't expect me to comment on everything, Baka!
 dlOptParser :: Parser DownloadOpts
 dlOptParser =
   DownloadOpts
     <$> argument str (metavar "PROVIDER" <> help "the name of the provider to retreive images from")
     <*> some (argument str (metavar "IDS" <> help "list of ids to download"))
-
-commandsParser :: Parser Commands
-commandsParser =
-  hsubparser
-    ( command "build" (info (pure Build) $ progDesc "build the image folder")
-        <> command "download" (info (Download <$> dlOptParser) $ progDesc "download images using IDS from a given PROVIDER")
-    )
