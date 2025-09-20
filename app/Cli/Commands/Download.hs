6e11c7d23da19e75267c0e1f9c20d5b5404ab61b
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-09-20 19:44:55 +0400

    wip: cli p7 handling invalid providers

--- app/Cli/Commands/Download.hs
+++ app/Cli/Commands/Download.hs
@@ -1,6 +1,6 @@
 module Cli.Commands.Download where
 
-import Booru.Builtin.Providers (danbooruDonmaiUs, safebooruOrg, specialUrls, zerochanNet)
+import Booru.Builtin.Providers (builtinProviders)
 import Booru.Core.Requests (getProviderMap, requestFile)
 import Booru.Schema.Images (
   Identifier (Id),
@@ -11,17 +11,23 @@ import Cli.Options (DownloadOpts (..))
 import Control.Monad (forM, forM_)
 import qualified Data.ByteString as L
 import qualified Data.Map as M
-import Data.Maybe (catMaybes)
+import Data.Maybe (catMaybes, fromMaybe)
 
 getImages :: DownloadOpts -> IO ()
 getImages DownloadOpts{provider = prv, ids = ids'} = do
   let
-    provMap = getProviderMap [danbooruDonmaiUs, specialUrls, safebooruOrg, zerochanNet]
+    provMap = getProviderMap builtinProviders
     idnfrs = map Id ids'
-  (Just fetchImage) <- return $ M.lookup prv provMap
+    fetchImage = fromMaybe (nullProvider prv) $ M.lookup prv provMap
+
   imgs' <- forM idnfrs fetchImage
   let imgs = catMaybes imgs'
   forM_ imgs downloadImage
+
+nullProvider :: String -> b -> IO (Maybe a)
+nullProvider prv = const $ do
+  putStrLn $ "Invalid Provider: " ++ prv
+  return Nothing
 
 downloadImage :: Image -> IO ()
 downloadImage img@Image{resolvedName = name} = do
