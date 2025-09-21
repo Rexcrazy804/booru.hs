99383584023efb1ce6fcb7d60a5c4b33e3330b90
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-09-21 12:24:27 +0400

    refactor(lib): relocated Identifiers into its own module

---
+++ lib/Booru/Schema/Identifier.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+
+module Booru.Schema.Identifier (
+  Identifier (..),
+  extractId,
+  extractId',
+) where
+
+import Data.Text (unpack)
+import GHC.Generics (Generic)
+import Toml.Schema
+
+data Identifier = Id String | WithNick {id :: String, nickname :: String}
+  deriving (Eq, Show, Generic)
+
+instance ToValue Identifier where
+  toValue (Id id') = toValue id'
+  toValue (WithNick id' nick) = toValue $ unwords [id', nick]
+
+instance FromValue Identifier where
+  fromValue (Text' l packed)
+    | [y, z] <- x = return $ WithNick{id = y, nickname = z}
+    | [x'] <- x = return $ Id x'
+    | otherwise = failAt l "Expected a string of '<id>' or '<id> <nickname>'"
+   where
+    x = words $ unpack packed
+  fromValue _ = fail "expected a string"
+
+extractId :: Identifier -> String
+extractId (Id x) = x
+extractId WithNick{id = x} = x
+
+-- returns a list, containing any matchable id information
+extractId' :: Identifier -> [String]
+extractId' (Id x) = [x]
+extractId' WithNick{id = x, nickname = n} = [x, n]
--- lib/Booru/Schema/Images.hs
+++ lib/Booru/Schema/Images.hs
@@ -1,19 +1,15 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingVia #-}
-{-# LANGUAGE DuplicateRecordFields #-}
 
 module Booru.Schema.Images (
   Images (..),
   Image (..),
   Tag,
-  Identifier (..),
-  extractId,
-  extractId',
 )
 where
 
+import Booru.Schema.Identifier (Identifier)
 import Booru.Schema.Providers (ProviderName)
-import Data.Text (unpack)
 import GHC.Generics (Generic)
 import Toml.Schema
 
@@ -43,28 +39,3 @@ data Image = Image
   }
   deriving (Eq, Show, Generic)
   deriving (ToTable, ToValue, FromValue) via GenericTomlTable Image
-
-data Identifier = Id String | WithNick {id :: String, nickname :: String}
-  deriving (Eq, Show, Generic)
-
-instance ToValue Identifier where
-  toValue (Id id') = toValue id'
-  toValue (WithNick id' nick) = toValue $ unwords [id', nick]
-
-instance FromValue Identifier where
-  fromValue (Text' l packed)
-    | [y, z] <- x = return $ WithNick{id = y, nickname = z}
-    | [x'] <- x = return $ Id x'
-    | otherwise = failAt l "Expected a string of '<id>' or '<id> <nickname>'"
-   where
-    x = words $ unpack packed
-  fromValue _ = fail "expected a string"
-
-extractId :: Identifier -> String
-extractId (Id x) = x
-extractId WithNick{id = x} = x
-
--- returns a list, containing any matchable id information
-extractId' :: Identifier -> [Tag]
-extractId' (Id x) = [x]
-extractId' WithNick{id = x, nickname = n} = [x, n]
