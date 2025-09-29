f9f93adda35d0f21edc9087e168d6e855dc7cfea
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-09-29 14:14:46 +0400

    feat(cli): improved getFname' that uses fullname for special providers

--- booru-cli/Cli/Utils/Build.hs
+++ booru-cli/Cli/Utils/Build.hs
@@ -7,7 +7,7 @@ import Booru.Core.Requests (requestFile)
 import Booru.Schema.Identifier (Identifier, extractId, toResolvedName)
 import Booru.Schema.Images (Image (Image, resolvedName), resolvedName)
 import qualified Booru.Schema.Images as Img
-import Booru.Schema.Providers (ProviderName)
+import Booru.Schema.Providers (Provider (file, name), ProviderName)
 import Booru.Schema.Sources (Source (Source, ids, provider))
 import qualified Booru.Schema.Sources as Src
 import Cli.Utils.Common (nullProvider)
@@ -57,20 +57,20 @@ getMetaData pmap Source{ids = idnfrs, provider = prv} = do
 - the function leveragtes 'Image.resolvedName' to identify existing files
 -}
 downloadImage :: FilePath -> Image -> IO ()
-downloadImage ddir img@Image{resolvedName = name} = do
-  let dwnPath = ddir </> name
+downloadImage ddir img@Image{resolvedName = name'} = do
+  let dwnPath = ddir </> name'
   imgExists <- doesFileExist dwnPath
   unless imgExists $ do
     rsbod <- requestFile img
-    putStrLn $ "Downloading: " ++ name
+    putStrLn $ "Downloading: " ++ name'
     L.writeFile dwnPath rsbod
 
 {- |
 First file path is a directory containing resolved images
 Second file path is the directory to plant (symlink) categorized images
 -}
-categoryToFs :: FilePath -> FilePath -> [Image] -> Category -> IO ()
-categoryToFs idir pdir imgs cat = do
+categoryToFs :: FilePath -> FilePath -> [Provider] -> [Image] -> Category -> IO ()
+categoryToFs idir pdir prvs imgs cat = do
   pdirExist <- doesDirectoryExist pdir
   when pdirExist $ putStrLn ("Deleting: " ++ pdir) >> removeDirectoryRecursive pdir
   attributeToFs (pdir </> "characters") $ characterC cat
@@ -78,6 +78,7 @@ categoryToFs idir pdir imgs cat = do
   attributeToFs (pdir </> "artists") $ artistC cat
  where
   rnMap = toRnameMap imgs
+  sPrvs = filterSpecialProviders prvs
 
   attributeToFs :: FilePath -> TagMap -> IO ()
   attributeToFs root tmap = forM_ (toList tmap) $ createTagFolder root
@@ -87,13 +88,27 @@ categoryToFs idir pdir imgs cat = do
         toSymLink rname =
           createFileLink
             (idir </> rname)
-            (tagF </> fromMaybe rname (Map.lookup rname rnMap >>= getFname))
+            (tagF </> fromMaybe rname (Map.lookup rname rnMap >>= getFname' sPrvs))
     putStrLn $ "Sylinking: " ++ tagF
     createDirectoryIfMissing True tagF
     forM_ rnames toSymLink
 
+filterSpecialProviders :: [Provider] -> [ProviderName]
+filterSpecialProviders prvs = map name $ filter (isNothing . file) prvs
+
 getFname :: Image -> Maybe String
 getFname Image{file = fURI'} = last . pathSegments <$> parseURI fURI'
+
+getFname' :: [ProviderName] -> Image -> Maybe String
+getFname' prvs Image{file = fURI', provider = prv, id = id'} = do
+  fURI <- parseURI fURI'
+  let fullName = last (pathSegments fURI)
+      (_, ftype) = span (/= '.') fullName
+  -- prvs here is a list of special providers
+  -- these providers don't have meaning full ids :<
+  if prv `elem` prvs
+    then return fullName
+    else return $ prv ++ extractId id' ++ ftype
 
 toRnameMap :: [Image] -> Map String Image
 toRnameMap img = fromList $ foldl (\acc cur -> (resolvedName cur, cur) : acc) [] img
