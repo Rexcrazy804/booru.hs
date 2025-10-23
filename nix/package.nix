f24a9a89bae3bdfb635e61f40874652dfb76d6c5
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-10-23 15:29:12 +0400

    chore(nix): updated package

--- nix/package.nix
+++ nix/package.nix
@@ -1,25 +1,25 @@
 {
   mkDerivation,
+  aeson,
+  aeson-qq,
   base,
-  lib,
-  toml-parser,
-  template-haskell,
-  text,
+  bytestring,
+  containers,
+  directory,
+  filepath,
+  hashable,
   hspec,
   hspec-discover,
-  aeson,
-  containers,
-  hashable,
-  http-conduit,
-  vector,
-  aeson-qq,
-  bytestring,
-  directory,
-  split,
-  filepath,
-  network-uri,
   http-client,
+  http-conduit,
+  lib,
+  network-uri,
   optparse-applicative_0_19_0_0,
+  split,
+  template-haskell,
+  text,
+  toml-parser,
+  vector,
 }: let
   inherit (lib.fileset) toSource unions fileFilter;
   hsfilter = fileFilter (file: lib.any file.hasExt ["hs"]);
@@ -45,28 +45,25 @@ in
     version = "1.0.1.2";
     isLibrary = true;
     isExecutable = true;
+    enableSeparateDataOutput = true;
     libraryHaskellDepends = [
       aeson
       base
       bytestring
       containers
+      directory
+      filepath
       hashable
+      http-client
       http-conduit
+      network-uri
+      optparse-applicative_0_19_0_0
       split
       text
       toml-parser
       vector
     ];
-    executableHaskellDepends = [
-      base
-      bytestring
-      containers
-      directory
-      filepath
-      http-client
-      optparse-applicative_0_19_0_0
-      network-uri
-    ];
+    executableHaskellDepends = [base];
     testHaskellDepends = [
       aeson
       aeson-qq
@@ -78,6 +75,7 @@ in
       toml-parser
     ];
     testToolDepends = [hspec-discover];
+    doHaddock = false;
     homepage = "https://github.com/Rexcrazy804/booru.hs";
     description = "Extensible Auto-categorizing image library";
     license = lib.licenses.mpl20;
